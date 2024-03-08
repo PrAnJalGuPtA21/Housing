52ce20803f1253ae3df65d71b0f48e234b9642ac
Pranjal Gupta <pranjal.g@codestoresolutions.com>
2024-03-08 18:06:06 +0530

    second

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,20 +1,26 @@
 import image from "../../assets/housing.png"
 import React from "react"
+import "./Hero.css"
 const Hero = () => {
     return (
         <>
-        <div style={{ position: "relative",display:"flex" }}>
-            <img src={image} style={{
-                display: "flex",
-                flexDirection: "row",
-                height: "700px",
-                marginLeft: "400px",
-            }} />
-            <div style={{position:"absolute", textAlign:"center",left:"200px",display:"flex" ,flexDirection:"column" ,alignItems:"center" }}>
-                <p style={{fontSize:"40px",fontWeight:"bold" , alignContent:"center" , alignItems:"center" , marginTop :"250px"}}>Find Your Dream House</p>
-                <p style={{fontSize:"40px",fontWeight:"bold" , alignContent:"center" , alignItems:"center" ,color:"#e07b39"}}>WITH US !!!</p>
+            <div style={{ position: "relative", display: "flex" }}>
+                <img src={image} style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    height: "700px",
+                    marginLeft: "400px",
+                }} />
+                <div style={{ position: "absolute", textAlign: "center", left: "200px", display: "flex", flexDirection: "column", alignItems: "center", marginTop: "230px" }}>
+                    <p style={{ fontSize: "40px", fontWeight: "bold", alignContent: "center", alignItems: "center" }}>Find Your Dream House</p>
+                    <p style={{ fontSize: "40px", fontWeight: "bold", alignContent: "center", alignItems: "center", color: "#e07b39" }}>WITH US !!!</p>
+                    <p style={{ fontSize: "13px", width: "420px" }}>Lookup for Residential properties for buying in India with 0% Brokerage,100% Genuine Owners. India's #1 Real Estate Property Website Without Brokers.</p>
+                    <button >
+                        Pricing
+                    </button>
+                    <button>Features</button>
                 </div>
-        </div>
+            </div>
         </>
     )
 }
