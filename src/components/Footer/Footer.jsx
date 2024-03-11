dba5b8cb8632de9c88d6e146811cbfb50f3ef1dc
Pranjal Gupta <pranjal.g@codestoresolutions.com>
2024-03-11 18:14:29 +0530

    change footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,60 +1,16 @@
+import { Link } from "react-router-dom"
 
 const Footer = () => {
     return (
-        <div className="container ms-6">
-            <footer className="py-5">
-                <div className="row">
-                    <div className="col-6 col-md-2 mb-3">
-                        <h5>Section</h5>
-                        <ul className="nav flex-column">
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
-                        </ul>
-                    </div>
-
-                    <div className="col-6 col-md-2 mb-3">
-                        <h5>Section</h5>
-                        <ul className="nav flex-column">
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
-                        </ul>
-                    </div>
-
-                    <div className="col-6 col-md-2 mb-3">
-                        <h5>Section</h5>
-                        <ul className="nav flex-column">
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
-                        </ul>
-                    </div>
-
-                    <div className="col-md-5 offset-md-1 mb-3">
-                        <form>
-                            <h5>Subscribe to our newsletter</h5>
-                            <p>Monthly digest of what's new and exciting from us.</p>
-                            <div className="d-flex flex-column flex-sm-row w-100 gap-2">
-                                <label htmlFor="newsletter1" className="visually-hidden">Email address</label>
-                                <input id="newsletter1" type="text" className="form-control" placeholder="Email address"/>
-                                    <button className="btn btn-primary"  type="button" style={{width:"150px" ,height:"40px"}}>Subscribe</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-
-                <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-                    <p>© 2024 Company, Inc. All rights reserved.</p>
-                    <ul className="list-unstyled d-flex">
-                        <li className="ms-3"><a className="link-body-emphasis" href="#"><svg className="bi" width="24" height="24"><use xlinkHref="#twitter"></use></svg></a></li>
-                        <li className="ms-3"><a className="link-body-emphasis" href="#"><svg className="bi" width="24" height="24"><use xlinkHref="#instagram"></use></svg></a></li>
-                        <li className="ms-3"><a className="link-body-emphasis" href="#"><svg className="bi" width="24" height="24"><use xlinkHref="#facebook"></use></svg></a></li>
-                    </ul>
-                </div>
+        <div className="container">
+            <footer className="py-3 my-4">
+                <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+                    <li className="nav-item"><Link to="/" style={{textDecoration:"none"}}><a href="#" className="nav-link px-2 text-body-secondary">Home</a></Link></li>
+                    <li className="nav-item"><Link to="/feature" style={{textDecoration:"none"}}><a href="#" className="nav-link px-2 text-body-secondary">Features</a></Link></li>
+                    <li className="nav-item"><Link to="/pricing" style={{textDecoration:"none"}}><a href="#" className="nav-link px-2 text-body-secondary">Pricing</a></Link></li>
+                    <li className="nav-item"><Link to="/home" style={{textDecoration:"none"}}><a href="#" className="nav-link px-2 text-body-secondary">About</a></Link></li>
+                </ul>
+                <p className="text-center text-body-secondary">© 2024 PG Development</p>
             </footer>
         </div>
     )
